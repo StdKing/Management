ae961bc816032a7aeef9b6bb06ff0ec4c94602c2
STD KING <112249837+StdKing@users.noreply.github.com>
2023-06-28 14:00:08 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -1089,7 +1089,7 @@ def main():
         except BadRequest as e:
             LOGGER.warning(e.message)
 
-    start_handler = CommandHandler("kalla", kalla)
+    start_handler = CommandHandler("start", start)
 
     help_handler = CommandHandler("help", get_help)
     help_callback_handler = CallbackQueryHandler(help_button, pattern=r"help_.*")
